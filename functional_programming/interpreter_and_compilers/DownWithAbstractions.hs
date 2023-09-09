30ca6c08bb07183cf583c5b65216b98662037d9f
Lorin-Lange <lo.-lange@arcor.de>
2023-09-09 13:29:12 +0200

    added template for some problems

---
+++ functional_programming/interpreter_and_compilers/DownWithAbstractions.hs
@@ -0,0 +1,11 @@
+----------------------------------------------------
+--                  HackerRank                    --
+--             Down With Abstractions             --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module DownWithAbstractions where
+
+main :: IO()
+main = do
+    print "Hello World!"
---
+++ functional_programming/interpreter_and_compilers/WhileLanguage.hs
@@ -0,0 +1,11 @@
+----------------------------------------------------
+--                  HackerRank                    --
+--                 While Language                 --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module WhileLanguage where
+
+main :: IO()
+main = do
+    print "Hello World!"
