07b6495b2d56e8cc3e055c456ef29c24d84c69d6
Lorin-Lange <lo.-lange@arcor.de>
2023-09-09 12:16:37 +0200

    added solution of HelloWorld and lambda_calculus-reductions_#1

---
+++ functional_programming/introduction/HelloWorld.hs
@@ -0,0 +1,8 @@
+module HelloWorld where
+
+hello_world :: IO()
+hello_world = putStrLn "Hello World"
+
+-- This part relates to Input/Output and can be used as it is. Do not modify this section
+main :: IO()
+main = hello_world
