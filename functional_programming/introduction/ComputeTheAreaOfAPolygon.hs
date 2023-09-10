a04aadf5ade86c2678ecfe3d30d33989fd3990de
Lorin-Lange <lo.-lange@arcor.de>
2023-09-10 20:20:42 +0200

    added solution of problem Compute the Area of a Polygon

---
+++ functional_programming/introduction/ComputeTheAreaOfAPolygon.hs
@@ -0,0 +1,23 @@
+----------------------------------------------------
+--                  HackerRank                    --
+--         Compute the Area of a Polygon          --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module Main where
+
+import Data.List.Split
+
+determinant :: (Int, Int) -> (Int, Int) -> Double
+determinant (x1, y1) (x2, y2) = fromIntegral $ x1 * y2 - x2 * y1
+
+shoelaceFormula :: [(Int, Int)] -> Double
+shoelaceFormula lst = (sum $ zipWith determinant lst (tail lst ++ [head lst])) / 2
+
+parseInput :: String -> [(Int, Int)]
+parseInput inp = 
+    let lst = tail $ lines inp
+    in map (\s -> let l = splitOn " " s in (read $ head l, read $ l !! 1)) lst
+
+main :: IO()
+main = interact $ show . shoelaceFormula . parseInput
