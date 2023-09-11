84ceb2fe7ba219b45deccecfc6f1693f603ef7ff
Lorin-Lange <lo.-lange@arcor.de>
2023-09-11 09:13:16 +0200

    added solution of problem Computing the GCD

---
+++ functional_programming/recursion/ComputingTheGCD.hs
@@ -0,0 +1,19 @@
+----------------------------------------------------
+--                  HackerRank                    --
+--               Computing the GCD                --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module Main where
+
+gcd' :: Integral a => a -> a -> a
+gcd' n 0 = n
+gcd' n m = gcd' m (n `mod` m)
+
+main :: IO()
+main = do
+  input <- getLine
+  print . uncurry gcd' . listToTuple . convertToInt . words $ input
+ where
+  listToTuple (x:xs:_) = (x,xs)
+  convertToInt = map (read :: String -> Int)
--- functional_programming/recursion/FibonacciNumbers.hs
+++ functional_programming/recursion/FibonacciNumbers.hs
@@ -5,14 +5,13 @@
 ----------------------------------------------------
 
 --Contributed by Ron Watkins
+
 module Main where
 
 fib :: Int -> Int
 fib n = last $ take n fibs
     where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
--- This part is related to the Input/Output and can be used as it is
--- Do not modify it
 main :: IO()
 main = do
     input <- getLine
