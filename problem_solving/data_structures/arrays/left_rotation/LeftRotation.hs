a88c7e710b327b52908d0c6ffedb5ac4f06726fa
Lorin-Lange <lo.-lange@arcor.de>
2024-06-01 12:02:38 +0200

    added solution for Left Rotation

---
+++ problem_solving/data_structures/arrays/left_rotation/LeftRotation.hs
@@ -0,0 +1,59 @@
+----------------------------------------------------
+--                   HackerRank                   --
+--                 Left Rotation                  --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+{-# LANGUAGE DuplicateRecordFields, FlexibleInstances, UndecidableInstances #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List 
+import Data.List.Split
+import Data.Set
+import Data.Text
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+--
+-- Complete the 'rotateLeft' function below.
+--
+-- The function is expected to return an INTEGER_ARRAY.
+-- The function accepts following parameters:
+--  1. INTEGER d
+--  2. INTEGER_ARRAY arr
+--
+
+rotateLeft :: Int -> [a] -> [a]
+rotateLeft d l = Data.List.drop d l ++ Data.List.take d l
+
+lstrip = Data.Text.unpack . Data.Text.stripStart . Data.Text.pack
+rstrip = Data.Text.unpack . Data.Text.stripEnd . Data.Text.pack
+
+main :: IO()
+main = do
+    stdout <- getEnv "OUTPUT_PATH"
+    fptr <- openFile stdout WriteMode
+
+    firstMultipleInputTemp <- getLine
+    let firstMultipleInput = Data.List.words $ rstrip firstMultipleInputTemp
+
+    let n = read (firstMultipleInput !! 0) :: Int
+
+    let d = read (firstMultipleInput !! 1) :: Int
+
+    arrTemp <- getLine
+
+    let arr = Data.List.map (read :: String -> Int) . Data.List.words $ rstrip arrTemp
+
+    let result = rotateLeft d arr
+
+    hPutStrLn fptr $ Data.List.intercalate " " $ Data.List.map (\x -> show x) $ result
+
+    hFlush fptr
+    hClose fptr
